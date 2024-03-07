b97a1842e9de78ce1bbbe82c1a0e824d71f88ab8
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-07 22:13:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function stringChop(str, size) {
   // your code here
+	const result = [];
+	let temp = '';
+	for (let i = 1; i <= str.length; i++) {
+		temp += str[i-1];
+		if (i%5===0) {
+			result.push(temp);
+			temp = '';
+		}
+		if (i===str.length) {
+			result.push(temp);
+		}
+	}
+	return result;
 }
 
 // Do not change the code below
